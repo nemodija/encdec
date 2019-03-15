03b5536ef22815409bd710b436aea6279c56ed31
nemodija <kenichiro.nemoto@gmail.com>
2019-03-15 01:58:15 +0000

    機能追加
    - XML整形
    - 変換結果から次の変換を行えるようにした

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,24 @@
+require 'rexml/document'
+require 'rexml/formatters/pretty'
+
 class HomeController < ApplicationController
+
+  class Input
+    attr_accessor :index, :text
+
+    def initialize(param = { index: -1, text: "" })
+      self.index = param[:index].to_i
+      self.text  = param[:text]
+    end
+  end
+
   def index
+    @input = Input.new
   end
 
   def convert
+
+    @input = Input.new(params[:input])
 
     rt = cgi_unescape_html(params[:input][:text]) if params[:CGIunescapeHTML].present?
     rt = cgi_unescape(params[:input][:text]) if params[:CGIunescape].present?
@@ -14,31 +30,10 @@ class HomeController < ApplicationController
     rt = base64_encode64(params[:input][:text]) if params[:BASE64encode64].present?
     rt = base64_decode64(params[:input][:text]) if params[:BASE64decode64].present?
 
-    @result = rt
-  end
+    rt = xml_formatting(params[:input][:text]) if params[:XMLformatting].present?
 
-  def cgi_unescape_html(value) 
-    lines = []
-    CGI.unescapeHTML(value).lines do |line|
-      lines.push line.chomp.gsub(/\\/, '\\\\\\')
-    end
-    lines.join('\\n')
-  end
-
-  def cgi_unescape(value) 
-    lines = []
-    CGI.unescape(value).lines do |line|
-      lines.push line.chomp.gsub(/\\/, '\\\\\\')
-    end
-    lines.join('\\n')
-  end
-
-  def uri_decode(value) 
-    lines = []
-    URI.decode(value).lines do |line|
-      lines.push line.chomp.gsub(/\\/, '\\\\\\')
-    end
-    lines.join('\\n')
+    @input.index += 1
+    @input.text = rt
   end
 
   def cgi_escape_html(value)
@@ -46,7 +41,15 @@ class HomeController < ApplicationController
     value.lines do |line|
       lines.push CGI.escapeHTML(line.chomp).gsub(/\\/, '\\\\\\')
     end
-    lines.join('\\n')
+    lines.join("\n")
+  end
+
+  def cgi_unescape_html(value) 
+    lines = []
+    CGI.unescapeHTML(value).lines do |line|
+      lines.push line.chomp.gsub(/\\/, '\\\\\\')
+    end
+    lines.join("\n")
   end
 
   def cgi_escape(value)
@@ -54,7 +57,15 @@ class HomeController < ApplicationController
     value.lines do |line|
       lines.push CGI.escape(line.chomp).gsub(/\\/, '\\\\\\')
     end
-    lines.join('\\n')
+    lines.join("\n")
+  end
+
+  def cgi_unescape(value) 
+    lines = []
+    CGI.unescape(value).lines do |line|
+      lines.push line.chomp.gsub(/\\/, '\\\\\\')
+    end
+    lines.join("\n")
   end
 
   def uri_encode(value)
@@ -62,14 +73,30 @@ class HomeController < ApplicationController
     value.lines do |line|
       lines.push URI.encode(line.chomp).gsub(/\\/, '\\\\\\')
     end
-    lines.join('\\n')
+    lines.join("\n")
+  end
+
+  def uri_decode(value) 
+    lines = []
+    URI.decode(value).lines do |line|
+      lines.push line.chomp.gsub(/\\/, '\\\\\\')
+    end
+    lines.join("\n")
   end
 
   def base64_encode64(value)
-    Base64.encode64(value).lines.map{|l|l.chomp}.join('\\n')
+    Base64.encode64(value).lines.map{|l|l.chomp}.join("\n")
   end
 
   def base64_decode64(value)
-    Base64.decode64(value).lines.map{|l|l.chomp}.join('\\n')
+    Base64.decode64(value).lines.map{|l|l.chomp.force_encoding("UTF-8")}.join("\n")
+  end
+
+  def xml_formatting(value)
+    doc = REXML::Document.new(value)
+    pretty_formatter = REXML::Formatters::Pretty.new
+    output = StringIO.new
+    pretty_formatter.write(doc, output)
+    output.string
   end
 end
